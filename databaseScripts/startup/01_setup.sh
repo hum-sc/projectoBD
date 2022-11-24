62ae32c30b01f5075a32da0df578476e47eecdcc
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-23 19:57:02 -0600

    Arreglando scripts de inicio

--- databaseScripts/startup/01_setup.sh
+++ databaseScripts/startup/01_setup.sh
@@ -1,6 +1,7 @@
 #! bin/bash
 for f in ~/sqlScripts/setup/*.sql
 do
+    ./setPassword.sh admin
     echo "Processing $f file..."
     # take action on each file. $f store current file name
     sqlplus sys/admin@XEPDB1 as sysdba @$f
