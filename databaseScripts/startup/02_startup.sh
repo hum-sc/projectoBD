2c95dd1e01e8151069211d94c622f215f79393f0
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-23 19:13:49 -0600

    todo arreglado

--- databaseScripts/startup/02_startup.sh
+++ databaseScripts/startup/02_startup.sh
@@ -1,7 +1,4 @@
 #! bin/bash
-
-# Execute the SQL scripts in /sqlScripts/setup
-# The SQL scripts are executed in alphabetical order
 for f in ~/sqlScripts/startup/*.sql
 do
   echo "Processing $f file..."
