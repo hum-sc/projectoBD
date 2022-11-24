db3c2a3cd07a3ced217d368e3bc24b5b7f47131a
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-23 19:25:45 -0600

    todo

--- databaseScripts/startup/01_setup.sh
+++ databaseScripts/startup/01_setup.sh
@@ -1,7 +1,5 @@
 #! bin/bash
 for f in ~/sqlScripts/setup/*.sql
 do
-  echo "Processing $f file..."
   sqlplus sys/admin@XEPDB1 as sysdba @$f
 done
-echo "Setup complete"
--- databaseScripts/startup/02_startup.sh
+++ databaseScripts/startup/02_startup.sh
@@ -1,7 +1,5 @@
 #! bin/bash
 for f in ~/sqlScripts/startup/*.sql
 do
-  echo "Processing $f file..."
   sqlplus dev/dev@XEPDB1 @$f
 done
-echo "StartUp complete"
