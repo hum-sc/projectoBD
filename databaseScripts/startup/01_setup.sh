382a1968ae45ce66a2837d3bdc7746a1126328d4
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-23 20:33:18 -0600

    arreglando todos los problemas

--- databaseScripts/startup/01_setup.sh
+++ databaseScripts/startup/01_setup.sh
@@ -1,8 +1,18 @@
 #! bin/bash
-for f in ~/sqlScripts/setup/*.sql
+red='\e[0;31m' NC='\e[0m'
+
+./setPassword.sh admin
+echo "${red}setup-------------------------------------${NC}"
+for f in ~/sqlScripts/setup/admin/*.sql
 do
-    ./setPassword.sh admin
-    echo "Processing $f file..."
-    # take action on each file. $f store current file name
+    echo "${red}Processing $f${NC}"
     sqlplus sys/admin@XEPDB1 as sysdba @$f
 done
+echo "${red}admin end----------------------------------------${NC}"
+for f in ~/sqlScripts/setup/*.sql
+do
+    echo "${red}Processing $f file.................................${NC}"
+    # take action on each file. $f store current file name
+    sqlplus dev/dev@XEPDB1 @$f
+done
+echo "${red}........................................................${NC}"
--- databaseScripts/startup/02_startup.sh
+++ databaseScripts/startup/02_startup.sh
@@ -1,7 +1,10 @@
 #! bin/bash
+red='\e[0;31m' NC='\e[0m'
+echo "startup!!!!!-------------------------------------------------------------------------------------"
 for f in ~/sqlScripts/startup/*.sql
 do
-    echo "Processing $f file..."
+    echo"${red}Processing $f${NC}"
     # take action on each file. $f store current file name
     sqlplus dev/dev@XEPDB1 @$f
 done
+echo "${red}........................................................${NC}"
