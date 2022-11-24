a1ae314235b8836930ec45f6526f88493819e9c7
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-23 19:46:12 -0600

    Arreglando scripts de inicio

--- databaseScripts/startup/01_setup.sh
+++ databaseScripts/startup/01_setup.sh
@@ -3,5 +3,5 @@ for f in ~/sqlScripts/setup/*.sql
 do
     echo "Processing $f file..."
     # take action on each file. $f store current file name
-    sqlplus sys/admin@1522/XEPDB1 as sysdba @$f
+    sqlplus sys/admin@XEPDB1 as sysdba @$f
 done
--- databaseScripts/startup/02_startup.sh
+++ databaseScripts/startup/02_startup.sh
@@ -3,5 +3,5 @@ for f in ~/sqlScripts/startup/*.sql
 do
     echo "Processing $f file..."
     # take action on each file. $f store current file name
-    sqlplus dev/dev@1522/XEPDB1 @$f
+    sqlplus dev/dev@XEPDB1 @$f
 done
