c6fc961f5cd3e69fd210f051c6a329bd386d37c4
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-26 17:29:42 -0600

    arreglando crud productos y finalizando crud clientes

--- sqlScripts/startup/03_crudCliente.sql
+++ sqlScripts/startup/03_crudCliente.sql
@@ -1,88 +1,150 @@
 -- Procedimiento de creacion de cliente
 CREATE OR REPLACE
-    PROCEDURE createCliente(rutP IN VARCHAR2, nombreP IN VARCHAR2, apellido_patP IN VARCHAR2,
-        apellido_matP IN VARCHAR2, domicilioP in VARCHAR2, telefonoP IN NUMBER)
+    PROCEDURE createCliente(rutP IN CLIENTE.RUT%TYPE,
+                            nombreP IN CLIENTE.NOMBRE%TYPE,
+                            apellido_patP IN CLIENTE.APELLIDO_PAT%TYPE,
+                            apellido_matP IN CLIENTE.APELLIDO_MAT%TYPE,
+                            domicilioP IN CLIENTE.DOMICILIO%TYPE,
+                            telefonoP IN CLIENTE.TELEFONO%TYPE)
     IS
-    rutU          VARCHAR2(12);
-    nombreU       VARCHAR2(30);
-    apellido_patU VARCHAR2(30);
-    apellido_matU VARCHAR2(30);
-    domicilioU    VARCHAR2(80);
-
 BEGIN
-    rutU := UPPER(rutP);
-    nombreU := UPPER(nombreP);
-    apellido_patU := UPPER(apellido_patP);
-    apellido_matU := UPPER(apellido_matP);
-    domicilioU := UPPER(domicilioP);
-INSERT INTO CLIENTE(RUT, NOMBRE, APELLIDO_PAT, APELLIDO_MAT, DOMICILIO, TELEFONO)
-VALUES (rutU, nombreU, apellido_patU, apellido_matU, domicilioU, telefonoP);
+    INSERT INTO CLIENTE(RUT, NOMBRE, APELLIDO_PAT, APELLIDO_MAT, DOMICILIO, TELEFONO)
+    VALUES (rutP, nombreP, apellido_patP, apellido_matP, domicilioP, telefonoP);
 END;
 /
 
 -- Procedmientos de actualizacion de cliente
 
 CREATE OR REPLACE
-    PROCEDURE updateNombreCliente(rutP IN VARCHAR2, nombreP IN VARCHAR2)
+    PROCEDURE updateNombreCliente(rutP IN CLIENTE.RUT%TYPE, nombreP IN CLIENTE.NOMBRE%TYPE)
     IS
-    nombreU VARCHAR2(30);
 BEGIN
-    nombreU := UPPER(nombreP);
     UPDATE CLIENTE
-        SET NOMBRE=nombreU
+        SET NOMBRE = nombreP
         WHERE RUT = rutP;
 END;
 /
 
 CREATE OR REPLACE
-    PROCEDURE updateApellidoPaternoCliente(rutP IN VARCHAR2, apellidoP IN VARCHAR2)
+    PROCEDURE updateApellidoPaternoCliente(rutP IN CLIENTE.RUT%TYPE, apellido IN CLIENTE.APELLIDO_PAT%TYPE)
     IS
-    rutU VARCHAR2(12);
-    apellido VARCHAR2(30);
 BEGIN
-    rutU := UPPER(rutP);
-    apellido := UPPER(apellidoP);
     UPDATE CLIENTE
         SET APELLIDO_PAT = apellido
-        WHERE RUT = rutU;
+        WHERE RUT = rutP;
 END;
 /
 
 CREATE OR REPLACE
-    PROCEDURE updateApellidoMaternoCliente(rutP IN VARCHAR2, apellidoP IN VARCHAR2)
+    PROCEDURE updateApellidoMaternoCliente(rutP IN CLIENTE.RUT%TYPE, apellido IN CLIENTE.APELLIDO_MAT%TYPE)
     IS
-    rutU VARCHAR2(12);
-    apellido VARCHAR(12);
 BEGIN
-    apellido := UPPER(apellidoP);
-    rutU := UPPER(rutP);
     UPDATE CLIENTE
         SET APELLIDO_MAT = apellido
-        WHERE RUT = rutU;
+        WHERE RUT = rutP;
 END;
 /
 
 CREATE OR REPLACE
-    PROCEDURE updateDomicilioCliente(rutP IN VARCHAR2, domicilioP IN VARCHAR2)
+    PROCEDURE updateDomicilioCliente(rutP IN CLIENTE.RUT%TYPE, domicilioP IN CLIENTE.DOMICILIO%TYPE)
     IS
-    rutU VARCHAR2(12);
-    domicilio VARCHAR (80);
 BEGIN
-    rutU := UPPER(rutP);
-    domicilio := UPPER(domicilioP);
     UPDATE CLIENTE
-        SET DOMICILIO = domicilio
-        WHERE RUT = rutU;
+    SET DOMICILIO = domicilioP
+    WHERE RUT = rutP;
 END;
 /
 
 CREATE OR REPLACE
-    PROCEDURE updateTelefonoCliente(rutP IN VARCHAR2, telefonoP IN NUMBER)
+    PROCEDURE updateTelefonoCliente(rutP IN CLIENTE.RUT%TYPE, telefonoP IN CLIENTE.TELEFONO%TYPE)
     IS
-    rutU VARCHAR2(12);
 BEGIN
-    rutU := UPPER(rutP);
     UPDATE CLIENTE
         SET TELEFONO = telefonoP
-        WHERE RUT = rutU;
+        WHERE RUT = rutP;
 END;
+/
+
+CREATE OR REPLACE
+    PROCEDURE deleteCliente(rutP IN CLIENTE.RUT%TYPE)
+    IS
+BEGIN
+    DELETE CLIENTE
+        WHERE RUT = rutP;
+END;
+/
+
+-- read cliente
+CREATE OR REPLACE
+    FUNCTION getClienteByRut(rutP IN CLIENTE.RUT%TYPE)
+    RETURN STRING
+    IS
+    consulta CLIENTE%rowtype;
+BEGIN
+    SELECT * INTO consulta
+        FROM CLIENTE
+        WHERE RUT = rutP;
+    RETURN consulta.RUT || ' ' || consulta.NOMBRE || ' ' || consulta.APELLIDO_PAT || ' ' || consulta.APELLIDO_MAT || ' ' || consulta.DOMICILIO || ' ' || consulta.TELEFONO||'';
+end;
+/
+
+CREATE OR REPLACE
+    FUNCTION getClienteByNombre(nombreP IN CLIENTE.NOMBRE%TYPE)
+    RETURN STRING
+    IS
+    consulta VARCHAR2(32767);
+    CURSOR tabla IS
+    SELECT * FROM CLIENTE
+        WHERE NOMBRE = upper(nombreP)
+    ORDER BY NOMBRE;
+BEGIN
+
+    consulta := RPAD('RUT', 12) || ' ' || RPAD('NOMBRE', 30) || ' ' || RPAD('APELLIDO PATERNO', 30)
+                || ' ' || RPAD('APELLIDO MATERNO', 30) || ' ' || RPAD('DOMICILIO',80) || ' ' || RPAD('TELEFONO',10)||CHR(10);
+    FOR row IN tabla
+        LOOP
+            consulta := consulta || ' ' || RPAD(row.RUT, 12)
+                                 || ' ' || RPAD(row.NOMBRE, 30)
+                                 || ' ' || RPAD(row.APELLIDO_PAT, 30)
+                                 || ' ' || RPAD(row.APELLIDO_MAT, 30)
+                                 || ' ' || RPAD(row.DOMICILIO, 80)
+                                 || ' ' || LPAD(row.TELEFONO, 10)
+                                 || ' ' || chr(10) ;
+        end loop;
+    return consulta;
+end;
+/
+
+CREATE OR REPLACE
+    FUNCTION getClientes(nombreP IN CLIENTE.NOMBRE%TYPE)
+    RETURN STRING
+    IS
+    consulta VARCHAR2(32767);
+    CURSOR tabla IS
+        SELECT * FROM CLIENTE
+        ORDER BY NOMBRE;
+BEGIN
+
+    consulta := RPAD('RUT', 12) || ' ' || RPAD('NOMBRE', 30) || ' ' || RPAD('APELLIDO PATERNO', 30)
+        || ' ' || RPAD('APELLIDO MATERNO', 30) || ' ' || RPAD('DOMICILIO',80) || ' ' || RPAD('TELEFONO',10)||CHR(10);
+    FOR row IN tabla
+        LOOP
+            consulta := consulta || ' ' || RPAD(row.RUT, 12)
+                || ' ' || RPAD(row.NOMBRE, 30)
+                || ' ' || RPAD(row.APELLIDO_PAT, 30)
+                || ' ' || RPAD(row.APELLIDO_MAT, 30)
+                || ' ' || RPAD(row.DOMICILIO, 80)
+                || ' ' || LPAD(row.TELEFONO, 10)
+                || ' ' || chr(10) ;
+        end loop;
+    return consulta;
+end;
+/
+
+CREATE OR REPLACE
+    PROCEDURE deleteCliente(rutP IN CLIENTE.RUT%TYPE)
+    IS
+BEGIN
+    DELETE FROM CLIENTE
+        WHERE RUT = rutP;
+end;
---
+++ sqlScripts/startup/04_crudProducto.sql
@@ -0,0 +1,99 @@
+-- Procedimiento de creacion de productos 
+CREATE OR REPLACE
+    PROCEDURE createProducto(codigoP IN PRODUCTO.CODIGO%TYPE, descripcionP IN PRODUCTO.DESCRIPCION%TYPE, precioUniP IN PRODUCTO.PRECIO_UNI%TYPE)
+    IS
+BEGIN
+    INSERT INTO PRODUCTO(CODIGO, DESCRIPCION, PRECIO_UNI)
+    VALUES (codigoP, descripcionP, precioUniP);
+END;
+/
+
+-- Procedmientos de actualizacion de productos
+
+CREATE OR REPLACE
+    PROCEDURE updateDescripcionProducto(codigoP IN PRODUCTO.CODIGO%TYPE, descripcionP IN PRODUCTO.DESCRIPCION%TYPE)
+    IS
+BEGIN
+    UPDATE PRODUCTO
+    SET DESCRIPCION = descripcionP
+    WHERE CODIGO = codigoP;
+END;
+/
+
+CREATE OR REPLACE
+    PROCEDURE updatePrecioUnitarioProducto(codigoP IN PRODUCTO.CODIGO%TYPE, precioU IN PRODUCTO.PRECIO_UNI%TYPE)
+    IS
+BEGIN
+    UPDATE PRODUCTO
+    SET PRECIO_UNI = precioU
+    WHERE CODIGO = codigoP;
+END;
+/
+
+-- Procedmientos de eliminacion de productos
+
+CREATE OR REPLACE
+    PROCEDURE deleteProducto(codP IN VARCHAR2)
+    IS
+
+BEGIN
+    DELETE FROM PRODUCTO
+    WHERE CODIGO = codP;
+END;
+/
+
+-- Procedmientos de consultar de productos
+
+CREATE OR REPLACE
+    FUNCTION getProductoByCodigo(codigoP IN VARCHAR2)
+    RETURN VARCHAR2
+    IS
+    consulta PRODUCTO%rowtype;
+BEGIN
+    SELECT * INTO consulta FROM PRODUCTO
+    WHERE CODIGO = codigoP;
+
+    RETURN consulta.CODIGO || ' ' || consulta.DESCRIPCION || ' ' || consulta.PRECIO_UNI;
+END;
+/
+
+CREATE OR REPLACE
+    FUNCTION getProductosBetweenPrecios(minimo IN PRODUCTO.PRECIO_UNI%TYPE, maximo IN PRODUCTO.PRECIO_UNI%TYPE)
+    RETURN VARCHAR2
+    IS
+    consulta VARCHAR2(32767);
+    CURSOR tabla IS
+        SELECT * FROM PRODUCTO
+            WHERE PRECIO_UNI BETWEEN minimo AND maximo;
+BEGIN
+    consulta := RPAD('CODIGO', 5) || ' ' || RPAD('DESCRIPCION', 70) || ' ' || LPAD('PRECIO UNITARIO',10) || CHR(10);
+    FOR row IN tabla
+        LOOP
+            consulta := consulta || ' ' || RPAD(row.CODIGO, 5)
+                                 || ' ' || RPAD(row.DESCRIPCION, 70)
+                                 || ' ' || LPAD(row.PRECIO_UNI, 10)
+                                 || CHR(10);
+        end loop;
+    RETURN consulta;
+end;
+/
+
+CREATE OR REPLACE
+    FUNCTION getProductos
+    RETURN VARCHAR2
+    IS
+    consulta VARCHAR2(32767);
+    CURSOR tabla IS
+        SELECT * FROM PRODUCTO;
+BEGIN
+    consulta := RPAD('CODIGO', 5) || ' ' || RPAD('DESCRIPCION', 70) || ' ' || LPAD('PRECIO UNITARIO',10) || CHR(10);
+    FOR row IN tabla
+        LOOP
+            consulta := consulta || ' ' || RPAD(row.CODIGO, 5)
+                || ' ' || RPAD(row.DESCRIPCION, 70)
+                || ' ' || LPAD(row.PRECIO_UNI, 10)
+                || CHR(10);
+        end loop;
+    RETURN consulta;
+end;
+/
