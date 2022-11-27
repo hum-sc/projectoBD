c80d69359b4a58cc937638421df522bc44910bc8
HectorCG8 <hec.cg8@gmail.com>
2022-11-26 19:18:02 -0600

    hola

--- sqlScripts/startup/createFactura.sql
+++ sqlScripts/startup/createFactura.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 CREATE OR REPLACE PROCEDURE pr_calcular_iva (numfac factura.num_fac%TYPE)
     IS
     costofinal NUMBER;
@@ -57,3 +58,59 @@ END;
 
 
 
+=======
+CREATE OR REPLACE 
+            PROCEDURE pr_create_factura(numV FACTURA.NUM_FAC%TYPE, 
+                       fechaV FACTURA.FECHA%TYPE, subV FACTURA.SUBTOTAL%TYPE,
+                       ivaV FACTURA.IVA%TYPE, totalV FACTURA.TOTAL%TYPE, 
+                       rutV FACTURA.RUT%TYPE)
+               IS 
+                 numU VARCHAR2(5);
+                 fechaU TO_DATE ('dd/mm/yyyy');
+                 subU NUMBER := 0;
+                 ivaU NUMBER := 0;
+                 totalU NUMBER := 0;
+                 rutV VARCHAR2(10);
+
+             BEGIN 
+                 numU := UPPER(numV);
+                 fechaU := UPPER(fechaV);
+                 subU := UPPER(subV);
+                 ivaU := UPPER(ivaV);
+                 totalU := UPPER(totalV);
+                 rutV := UPPER(rutV);
+
+INSERT INTO FACTURA(NUM_FAC, FECHA, SUBTOTAL, IVA, TOTAL, RUT)
+             VALUES (numU, fechaU, subU, ivaU, totalU, rutV);
+
+END pr_create_factura;
+
+//
+
+CREATE OR REPLACE 
+          PROCEDURE pr_create_Venta
+          (rCantidad        IN VENTA.CANTIDAD%TYPE,
+           rNumF            IN VENTA.NUM_FAC%TYPE,
+           rCodigo          IN VENTA.CODIGO%TYPE
+          )
+          
+          IS 
+          
+BEGIN 
+    INSERT INTO VENTA (cantidad, 
+                num_fac,
+                codigo)
+    VALUES (rCantidad,
+            rNumF,
+            rCodigo);
+            
+END pr_create_Venta;
+/
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
