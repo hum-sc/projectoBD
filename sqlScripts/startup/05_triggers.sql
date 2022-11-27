42f7a4be5d9f31eda240cb4b830e5a4bdcc0c05c
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-26 20:29:34 -0600

    a

--- sqlScripts/startup/05_triggers.sql
+++ sqlScripts/startup/05_triggers.sql
@@ -27,7 +27,31 @@ END;
 
 CREATE OR REPLACE
     TRIGGER calcularIVA
-    BEFORE INSERT VENTA ON FACTURA
+    BEFORE INSERT ON VENTA
+    DECLARE
+        newIva FACTURA.IVA%TYPE;
+        newSubTotal FACTURA.SUBTOTAL%TYPE;
+        newTotalFactura FACTURA.TOTAL%TYPE;
+        subtotalFactura FACTURA.SUBTOTAL%TYPE;
+        totalFactura FACTURA.TOTAL%TYPE;
+        factura FACTURA%rowtype;
+        precioUnitario PRODUCTO.PRECIO_UNI%TYPE;
+BEGIN
+        SELECT PRECIO_UNI INTO precioUnitario FROM PRODUCTO
+            WHERE CODIGO = NEW.CODIGO;
+        NEW.TOTAL := precioUnitario* NEW.CANTIDAD;
+        SELECT * INTO factura FROM FACTURA
+            WHERE NUM_FAC = NEW.NUM_FAC;
+        newSubTotal := factura.SUBTOTAL + NEW.TOTAL;
+        newTotalFactura := newSubTotal*1.16;
+        newIva := newTotalFactura - newSubTotal;
+        UPDATE FACTURA
+            SET SUBTOTAL = newSubTotal,
+                TOTAL = newTotalFactura,
+                IVA = newIva
+        WHERE
+
+end;
 
 
 
