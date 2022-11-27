147c98dd1e35761b34df41f6ae385a9574b008c7
Humberto Antonio Salinas Cortés <humbertoantsc@gmail.com>
2022-11-26 19:32:36 -0600

    cambios menores

--- sqlScripts/startup/06_createFactura.sql
+++ sqlScripts/startup/06_createFactura.sql
@@ -1,26 +1,20 @@
 CREATE OR REPLACE
             PROCEDURE pr_create_factura(numV FACTURA.NUM_FAC%TYPE, 
-                       fechaV FACTURA.FECHA%TYPE, subV FACTURA.SUBTOTAL%TYPE,
-                       ivaV FACTURA.IVA%TYPE, totalV FACTURA.TOTAL%TYPE, 
+                       fechaV FACTURA.FECHA%TYPE,
                        rutV FACTURA.RUT%TYPE)
                IS 
                  numU VARCHAR2(5);
-                 fechaU TO_DATE ('dd/mm/yyyy');
                  subU NUMBER := 0;
                  ivaU NUMBER := 0;
                  totalU NUMBER := 0;
-                 rutV VARCHAR2(10);
+                 rutU VARCHAR2(10);
 
              BEGIN 
                  numU := UPPER(numV);
-                 fechaU := UPPER(fechaV);
-                 subU := UPPER(subV);
-                 ivaU := UPPER(ivaV);
-                 totalU := UPPER(totalV);
-                 rutV := UPPER(rutV);
+                 rutU := UPPER(rutV);
 
 INSERT INTO FACTURA(NUM_FAC, FECHA, SUBTOTAL, IVA, TOTAL, RUT)
-             VALUES (numU, fechaU, subU, ivaU, totalU, rutV);
+             VALUES (numU, fechaV, 0,0,0, rutU);
 
 END pr_create_factura;
 
