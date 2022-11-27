b5af7ab4fc29558fca551a61d30a659737a48764
Humberto SC <hsalinasc001@alumno.uaemex.mx>
2022-11-26 20:54:50 -0600

    Humberto (#7)
    * docker puerto personal
    
    * CRUD de los Productos
    
    * arreglando crud productos y finalizando crud clientes
    
    * CORRIGIENDO
    
    * Mi parte de la gui
    
    * a
    
    Co-authored-by: CesarIRM19 <navirasec1.0@gmail.com>
    Co-authored-by: Dylan Ramos <dylandelarosa06@gmail.com>

--- sqlScripts/startup/05_triggers.sql
+++ sqlScripts/startup/05_triggers.sql
@@ -5,6 +5,7 @@ CREATE OR REPLACE
     ON CLIENTE
     FOR EACH ROW
 BEGIN
+    :NEW.RUT := UPPER(:NEW.RUT);
     :NEW.NOMBRE := UPPER(:NEW.NOMBRE);
     :NEW.APELLIDO_PAT := UPPER(:NEW.APELLIDO_PAT);
     :NEW.APELLIDO_MAT := UPPER(:NEW.APELLIDO_MAT);
@@ -21,7 +22,35 @@ CREATE OR REPLACE
 BEGIN
     :NEW.DESCRIPCION := UPPER(:NEW.DESCRIPCION);
 END;
+/
 
+CREATE OR REPLACE
+    TRIGGER calcularIVA
+    BEFORE INSERT ON VENTA
+    DECLARE
+        newIva FACTURA.IVA%TYPE;
+        newSubTotal FACTURA.SUBTOTAL%TYPE;
+        newTotalFactura FACTURA.TOTAL%TYPE;
+        subtotalFactura FACTURA.SUBTOTAL%TYPE;
+        totalFactura FACTURA.TOTAL%TYPE;
+        factura FACTURA%rowtype;
+        precioUnitario PRODUCTO.PRECIO_UNI%TYPE;
+BEGIN
+        SELECT PRECIO_UNI INTO precioUnitario FROM PRODUCTO
+            WHERE CODIGO = NEW.CODIGO;
+        NEW.TOTAL := precioUnitario* NEW.CANTIDAD;
+        SELECT * INTO factura FROM FACTURA
+            WHERE NUM_FAC = NEW.NUM_FAC;
+        newSubTotal := factura.SUBTOTAL + NEW.TOTAL;
+        newTotalFactura := newSubTotal*1.16;
+        newIva := newTotalFactura - newSubTotal;
+        UPDATE FACTURA
+            SET SUBTOTAL = newSubTotal,
+                TOTAL = newTotalFactura,
+                IVA = newIva
+        WHERE
+
+end;
 
 
 
